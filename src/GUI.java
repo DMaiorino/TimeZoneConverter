e970cb0ced5591c7ec6eaee427ced537e88b286f
David Maiorino <maiorinodavid@gmail.com>
2013-10-28 00:14:54 +0900

    Added menu to add different timezone selections.

--- src/GUI.java
+++ src/GUI.java
@@ -1,14 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
 import org.jdesktop.swingx.*;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemListener;
-import java.awt.event.KeyEvent;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
+import java.awt.event.*;
+import java.util.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -39,21 +33,31 @@ public class GUI {
     JLabel newDateLabel;
     JLabel newTimeLabel;
 
+    //Menu Components
+    JMenuBar menuBar;
+    JMenu menu;
+    JMenuItem menuItem;
+    JRadioButtonMenuItem standardTimezone;
+    JRadioButtonMenuItem allTimezone;
+    GUI gui;
+    ItemListener standardListener;
+    ItemListener allListener;
+
+
     //Time Info
     String currentTZ = Calendar.getInstance().getTimeZone().getID();
-    String[] timezoneList = {"Africa/Algiers", "Africa/Cairo", "Africa/Casablanca", "Africa/Harare", "Africa/Nairobi", "Africa/Windhoek", "America/Bogota", "America/Buenos_Aires", "America/Caracas", "America/Chihuahua", "America/Guatemala", "America/Mexico_City", "America/Montevideo", "America/Santiago", "America/Tijuana", "Asia/Amman", "Asia/Baghdad", "Asia/Baku", "Asia/Bangkok", "Asia/Beirut", "Asia/Calcutta", "Asia/Colombo", "Asia/Dhaka", "Asia/Hong_Kong", "Asia/Irkutsk", "Asia/Jerusalem", "Asia/Kabul", "Asia/Karachi", "Asia/Katmandu", "Asia/Krasnoyarsk", "Asia/Kuala_Lumpur", "Asia/Kuwait", "Asia/Magadan", "Asia/Muscat", "Asia/Novosibirsk", "Asia/Rangoon", "Asia/Seoul", "Asia/Taipei", "Asia/Tbilisi", "Asia/Tehran", "Asia/Tokyo", "Asia/Vladivostok", "Asia/Yakutsk", "Asia/Yekaterinburg", "Asia/Yerevan", "Atlantic/Azores", "Atlantic/Cape_Verde", "Australia/Adelaide", "Australia/Brisbane", "Australia/Darwin", "Australia/Hobart", "Australia/Perth", "Australia/Sydney", "Brazil/East", "Canada/Eastern", "Canada/Newfoundland", "Canada/Saskatchewan", "Europe/Athens", "Europe/Belgrade", "Europe/Berlin", "Europe/Brussels", "Europe/Helsinki", "Europe/London", "Europe/Minsk", "Europe/Moscow", "Europe/Paris", "Europe/Warsaw", "Pacific/Auckland", "Pacific/Fiji", "Pacific/Guam", "Pacific/Midway", "US/Alaska", "US/Arizona", "US/Central", "US/East-Indiana", "US/Eastern", "US/Hawaii", "US/Mountain", "US/Pacific", "UTC"} ;
-
+    String[] allTimezoneList  = TimeZone.getAvailableIDs();
+    String[] standardTimezoneList = {"Africa/Algiers", "Africa/Cairo", "Africa/Casablanca", "Africa/Harare", "Africa/Nairobi", "Africa/Windhoek", "America/Bogota", "America/Buenos_Aires", "America/Caracas", "America/Chihuahua", "America/Guatemala", "America/Mexico_City", "America/Montevideo", "America/Santiago", "America/Tijuana", "Asia/Amman", "Asia/Baghdad", "Asia/Baku", "Asia/Bangkok", "Asia/Beirut", "Asia/Calcutta", "Asia/Colombo", "Asia/Dhaka", "Asia/Hong_Kong", "Asia/Irkutsk", "Asia/Jerusalem", "Asia/Kabul", "Asia/Karachi", "Asia/Katmandu", "Asia/Krasnoyarsk", "Asia/Kuala_Lumpur", "Asia/Kuwait", "Asia/Magadan", "Asia/Muscat", "Asia/Novosibirsk", "Asia/Rangoon", "Asia/Seoul", "Asia/Taipei", "Asia/Tbilisi", "Asia/Tehran", "Asia/Tokyo", "Asia/Vladivostok", "Asia/Yakutsk", "Asia/Yekaterinburg", "Asia/Yerevan", "Atlantic/Azores", "Atlantic/Cape_Verde", "Australia/Adelaide", "Australia/Brisbane", "Australia/Darwin", "Australia/Hobart", "Australia/Perth", "Australia/Sydney", "Brazil/East", "Canada/Eastern", "Canada/Newfoundland", "Canada/Saskatchewan", "Europe/Athens", "Europe/Belgrade", "Europe/Berlin", "Europe/Brussels", "Europe/Helsinki", "Europe/London", "Europe/Minsk", "Europe/Moscow", "Europe/Paris", "Europe/Warsaw", "Pacific/Auckland", "Pacific/Fiji", "Pacific/Guam", "Pacific/Midway", "US/Alaska", "US/Arizona", "US/Central", "US/East-Indiana", "US/Eastern", "US/Hawaii", "US/Mountain", "US/Pacific", "UTC"} ;
 
     public void start(){
-
 
         mainPanel = new Panel();
         Menu menu = new Menu();
         GridBagConstraints c = new GridBagConstraints();
+
         c.insets = new Insets(0,10,0,10);
         c.weightx = 0.5;
         c.weighty = 0.5;
-
 
         //Add the base time components
         baseLabel = new JLabel("Current Time", SwingConstants.CENTER);
@@ -62,8 +66,7 @@ public class GUI {
         c.gridy = 0;
         mainPanel.add(baseLabel, c);
 
-        baseTimezoneBox = new JComboBox(timezoneList);
-        baseTimezoneBox.setSelectedItem(currentTZ);
+        baseTimezoneBox = new JComboBox(standardTimezoneList);
         c.gridy = 1;
         mainPanel.add(baseTimezoneBox, c);
 
@@ -84,8 +87,7 @@ public class GUI {
         c.gridy = 0;
         mainPanel.add(newLabel, c);
 
-        newTimezoneBox = new JComboBox(timezoneList);
-        newTimezoneBox.setSelectedItem("UTC");
+        newTimezoneBox = new JComboBox(standardTimezoneList);
         c.gridy = 1;
         mainPanel.add(newTimezoneBox, c);
 
@@ -109,15 +111,30 @@ public class GUI {
 
         //Setup the Frame
         frame = new Frame("Timezone Converter");
-        frame.setJMenuBar(menu.createMenuBar());
+        frame.setJMenuBar(createMenuBar());
         frame.getContentPane().add(mainPanel);
         frame.pack();
 
         //Update the new time once (Currently set to UTC
         convertTime();
-
     }
 
+    public void setTimezoneList(String[] timezoneList){
+
+        Arrays.sort(timezoneList);
+
+        baseTimezoneBox.removeAllItems();
+        newTimezoneBox.removeAllItems();
+        for (String tz : timezoneList){
+            baseTimezoneBox.addItem(tz);
+            newTimezoneBox.addItem(tz);
+        }
+
+        baseTimezoneBox.setSelectedItem(currentTZ);
+        newTimezoneBox.setSelectedItem("UTC");
+        convertTime(); //Convert to set for new values.
+
+    }
 
     public void convertTime(){
 
@@ -166,10 +183,6 @@ public class GUI {
 
     }
 
-
-
-
-
     public class converterAction implements ActionListener{
 
         public void actionPerformed(ActionEvent a){
@@ -177,5 +190,75 @@ public class GUI {
         }
 
     }
+
+
+    //Adding menu here
+
+    public JMenuBar createMenuBar() {
+
+        menu = new JMenu("Menu");
+        menuBar = new JMenuBar();
+        standardListener = new standardListener();
+        allListener = new allListener();
+
+        //a group of check box menu items
+        standardTimezone = new JRadioButtonMenuItem("Regular Timezone");
+        standardTimezone.setMnemonic(KeyEvent.VK_S);
+        standardTimezone.addItemListener(standardListener);
+        menu.add(standardTimezone);
+
+        allTimezone = new JRadioButtonMenuItem("All Timezone");
+        allTimezone.setMnemonic(KeyEvent.VK_A);
+        allTimezone.addItemListener(allListener);
+        menu.add(allTimezone);
+
+        menuItem = new JMenuItem("Quit", KeyEvent.VK_Q);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.CTRL_MASK));
+        menuItem.addActionListener(new quitListener());
+
+        menu.addSeparator();
+        menu.add(menuItem);
+        menuBar.add(menu);
+        standardTimezone.setSelected(true);
+        return menuBar;
+
+    }
+
+    public JRadioButtonMenuItem getStandardTimezone(){
+        return standardTimezone;
+    }
+
+    public JRadioButtonMenuItem getAllTimezone(){
+        return allTimezone;
+    }
+
+    public class quitListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            System.exit(0);
+        }
+    }
+
+    public class allListener implements ItemListener{
+        public  void itemStateChanged(ItemEvent e){
+            if (standardTimezone.isSelected()){
+                standardTimezone.removeItemListener(standardListener);
+                standardTimezone.setSelected(false);
+                standardTimezone.addItemListener(standardListener);
+            }
+            setTimezoneList(allTimezoneList);
+        }
+    }
+
+    public class standardListener implements ItemListener{
+        public  void itemStateChanged(ItemEvent e){
+            if (allTimezone.isSelected()){
+                allTimezone.removeItemListener(allListener);
+                allTimezone.setSelected(false);
+                allTimezone.addItemListener(allListener);
+            }
+            setTimezoneList(standardTimezoneList);
+        }
+    }
+
 
 }
