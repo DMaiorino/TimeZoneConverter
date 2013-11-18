f3136a91fc22e5f94bd805e1822b429da633538e
David Maiorino <maiorinodavid@gmail.com>
2013-11-18 23:57:56 +0900

    Initial check-in for font resizing. This needs some work.

--- src/GUI.java
+++ src/GUI.java
@@ -1,5 +1,9 @@
+import org.jdesktop.swingx.JXDatePicker;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 
 /**
  * Created with IntelliJ IDEA.
@@ -9,7 +13,7 @@ import java.awt.*;
  * A simple app for converting time.
  */
 
-public class GUI {
+public class GUI implements ComponentListener{
 
 
     //Window
@@ -27,12 +31,38 @@ public class GUI {
     Panel panel;
     JPanel mainPanel;
 
+    //Labels
+    JLabel newDateLabel;
+    JLabel newTimeLabel;
+
+    //Spinners
+    JSpinner baseTimeSpinner;
+
+    //Button
+    JButton convertButton;
+
+    //DatePicker
+    JXDatePicker baseDatePicker;
+
     public void start(){
 
         //Create panel
         mainPanel = new JPanel();
         panel = new Panel();
         mainPanel = panel.getPanel();
+
+        //Get Labels
+        newDateLabel = panel.getNewDateLabel();
+        newTimeLabel = panel.getNewTimeLabel();
+
+        //Get Spinner
+        baseTimeSpinner = panel.getBaseTimeSpinner();
+
+        //Button
+        convertButton = panel.getConvertButton();
+
+        // Get DatePicker
+        baseDatePicker = panel.getBaseDatePicker();
 
         this.baseTimezoneBox = panel.getBaseTimezoneBox();
         this.newTimezoneBox = panel.getNewTimezoneBox();
@@ -54,6 +84,58 @@ public class GUI {
         //Update the new time once (Currently set to UTC )
         panel.convertTime();
         frame.setSize(new Dimension(400,250));
+        frame.addComponentListener(this);
+
     }
+
+
+    public void componentHidden(ComponentEvent e) {
+        //
+    }
+
+    public void componentMoved(ComponentEvent e) {
+        //
+    }
+
+    public void componentResized(ComponentEvent e) {
+
+        int fontSize = resizeLabelFont(newDateLabel);
+
+        Font resizeFont = new Font("New Times Roman",Font.BOLD,fontSize);
+
+        newDateLabel.setFont(resizeFont);
+        newTimeLabel.setFont(resizeFont);
+        baseTimezoneBox.setFont(resizeFont);
+        newTimezoneBox.setFont(resizeFont);
+        convertButton.setFont(resizeFont);
+        baseDatePicker.setFont(resizeFont);
+        panel.setBaseTimeSpinnerFont(fontSize);
+
+    }
+
+    public void componentShown(ComponentEvent e) {
+        //
+    }
+
+    private int resizeLabelFont(JLabel label){
+
+        Font labelFont = label.getFont();
+        String labelText = label.getText();
+
+        int stringWidth = label.getFontMetrics(labelFont).stringWidth(labelText);
+        int componentWidth = label.getWidth();
+
+        // Find out how much the font can grow in width.
+        double widthRatio = (double)componentWidth / (double)stringWidth;
+        int newFontSize = (int)(labelFont.getSize() * widthRatio) -4 ;
+        int componentHeight = label.getHeight();
+
+        // Pick a new font size so it will not be larger than the height of label.
+        int fontSizeToUse = Math.min(newFontSize, componentWidth);
+
+        return fontSizeToUse;
+
+    }
+
 
 }
